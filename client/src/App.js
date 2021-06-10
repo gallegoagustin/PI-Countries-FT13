53154cc29b3f85af185e5a9abe792b1d96ceee5e
gallegoagustin <gallegoagustin7@gmail.com>
2021-06-10 11:49:10 -0300

    done nav & footer

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,18 @@
 import React from 'react';
 import { Route } from 'react-router-dom';
 import Landing from './pages/landing/Landing.jsx';
+import Home from './pages/home/Home.jsx';
+import NavBar from './components/nav/NavBar.jsx';
+import Footer from './components/footer/Footer.jsx';
 import './App.css';
 
 function App() {
   return (
     <div>
-      <Route path='/' component={Landing}/>
+      <Route exact path='/' component={Landing}/>
+      <Route path='/home' component={NavBar}/>
+      <Route path='/home' component={Home}/>
+      <Route path='/home' component={Footer}/>
     </div>
   );
 }
--- client/src/components/starter-box/StarterBox.jsx
+++ client/src/components/starter-box/StarterBox.jsx
@@ -9,7 +9,7 @@ export default function
             <h1>Countries App</h1>
             <hr className={styles.separator}></hr>
             <p>This app was built as a learning project for <strong>Henry's bootcamp</strong>. You will be able to look for information about any country in the world and add touristic activities for them.</p>
-            <p>Not only you can <strong>search for countries</strong> and <strong>add activities</strong> but also ordering or filtering them by name and kind of activity if necessary.</p>
+            <p>Not only you can <strong>search for countries</strong> and <strong>add activities</strong> but also ordering or filtering them by name, continent and kind of activity if necessary.</p>
             <p>You can check the <strong>responsive design</strong> too!</p>
             <Link to='/home'><a className={styles.getStartedBox}><span>Get started</span></a></Link>
         </div>
