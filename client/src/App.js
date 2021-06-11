abea6ca7902a7776a96ea575350f2c3a5339ae64
gallegoagustin <gallegoagustin7@gmail.com>
2021-06-11 18:06:16 -0300

    done add-activity form

--- client/src/App.js
+++ client/src/App.js
@@ -2,6 +2,8 @@ import React from 'react';
 import { Route } from 'react-router-dom';
 import Landing from './pages/landing/Landing.jsx';
 import Home from './pages/home/Home.jsx';
+import About from './pages/about/About.jsx';
+import Activity from './pages/activity-form/Activity.jsx';
 import Detail from './pages/country-detail/Detail.jsx';
 import NavBar from './components/nav/NavBar.jsx';
 import Footer from './components/footer/Footer.jsx';
@@ -11,10 +13,12 @@ function App() {
   return (
     <div>
       <Route exact path='/' component={Landing}/>
-      <Route path='/home' component={NavBar}/>
-      <Route exact path='/home' component={Home}/>
-      <Route path='/home/:id' component={Detail}/>
-      <Route path='/home' component={Footer}/>
+      <Route path='/:s' component={NavBar}/>
+      <Route path='/home' component={Home}/>
+      <Route path='/about' component={About}/>
+      <Route path='/detail/:id' component={Detail}/>
+      <Route path='/activity' component={Activity}/>
+      <Route path='/:s' component={Footer}/>
     </div>
   );
 }
--- client/src/pages/country-detail/Detail.jsx
+++ client/src/pages/country-detail/Detail.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { connect } from 'react-redux';
+import DetailCard from '../../components/detail-card/DetailCard';
 import styles from './Detail.module.css';
 
 function Detail(props) {
@@ -10,19 +11,7 @@ function Detail(props) {
         <div className={styles.detailContainer}>
             <h1>Detailed information for {props.detail.name}</h1>
             <hr className={styles.separator} />
-            <div className={styles.detailRow}>
-                <div>
-                    <img className={styles.detailFlag} src={props.detail.flag} alt="" />
-                </div>
-                <div className={styles.detailText}>
-                    <p><strong>Name:</strong> {props.detail.name} ({props.detail.id})</p>
-                    <p><strong>Continent:</strong> {props.detail.continent}</p>
-                    <p><strong>Capital:</strong> {props.detail.capital}</p>
-                    <p><strong>Subregion:</strong> {props.detail.subregion}</p>
-                    <p><strong>Area:</strong> {props.detail.area} km2</p>
-                    <p><strong>Population:</strong> {props.detail.population} citizens</p>
-                </div>
-            </div>
+            <DetailCard />
             <div className={styles.activitiesColumn}>
                 <p className={styles.activitiesTitle}><strong>Touristic activities</strong></p>
                 <div className={styles.activitiesContainer}>
@@ -32,7 +21,7 @@ function Detail(props) {
                             <div className={styles.activityBox}>
                                 <strong>Name:</strong> {a.name}<br></br>
                                 <strong>Level:</strong> {a.level}/5<br></br>
-                                <strong>Length:</strong> {a.length} hours<br></br>
+                                <strong>Length:</strong> {a.length} hour(s)<br></br>
                                 <strong>Season:</strong> {a.season}<br></br>
                             </div>
                         )
